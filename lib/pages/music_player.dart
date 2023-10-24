44932f64e65b91fbe9b3a7b3ae30e4d050aecfe4
jinholee <jinholee@student.42seoul.kr>
2023-10-24 21:52:38 +0200

    feat: music_player with media_kit instead

--- lib/pages/music_player.dart
+++ lib/pages/music_player.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_head_unit/ui/music_player.dart';
-import 'package:media_kit/media_kit.dart';
 
 class MusicInfo {
   const MusicInfo(this.title, this.url);
@@ -11,34 +10,6 @@ class MusicInfo {
 
 class MusicPlayer extends StatefulWidget {
   const MusicPlayer({super.key});
-
-  final List<MusicInfo> fromStorage = const [
-    MusicInfo('nyancat',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('chrome_cast',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3')
-  ];
-
-  final List<MusicInfo> fromServer = const [
-    MusicInfo('nyancat',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('chrome_cast',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('what',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('is',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('this',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('list',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('going',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('to',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3'),
-    MusicInfo('do',
-        'https://github.com/Lagavulin9/media_repo/raw/main/audios/happy-day.mp3')
-  ];
 
   @override
   State<MusicPlayer> createState() => _MusicPlayerState();
--- lib/ui/music_player.dart
+++ lib/ui/music_player.dart
@@ -1,53 +1,56 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:metadata_god/metadata_god.dart';
 import 'package:rxdart/rxdart.dart';
 
 class MetadataWidget extends StatelessWidget {
-  const MetadataWidget(
-      {super.key,
-      required this.image,
-      required this.title,
-      required this.artist});
+  const MetadataWidget({super.key, required this.player});
 
-  final Widget image;
-  final String title;
-  final String artist;
+  final Player player;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        DecoratedBox(
-          decoration: BoxDecoration(
-            boxShadow: const [
-              BoxShadow(
-                  color: Colors.black12, offset: Offset(2, 4), blurRadius: 4)
-            ],
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: image,
-          ),
-        ),
-        SizedBox(height: 20),
-        Text(
-          title,
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 8),
-        Text(
-          artist,
-          style: TextStyle(fontSize: 16),
-        ),
-        SizedBox(height: 10)
-      ],
-    );
+    return StreamBuilder(
+        stream: player.stream.playlist,
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return const Text("No media");
+          }
+          final List<Media> _playlists = snapshot.data!.medias;
+          final int _current_index = snapshot.data!.index;
+          final String _selected_file = _playlists[_current_index].uri;
+          return FutureBuilder(
+              future: MetadataGod.readMetadata(file: _selected_file),
+              builder: (context, snapshot) {
+                final metadata = snapshot.data;
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    metadata?.picture == null
+                        ? Text("No image")
+                        : Image(
+                            image: MemoryImage(metadata!.picture!.data),
+                            width: 250,
+                            height: 250,
+                          ),
+                    const SizedBox(height: 15),
+                    Text(
+                      "${metadata?.title ?? 'Unknown Title'}",
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      "${metadata?.artist ?? 'Unknown Artist'}",
+                      style: const TextStyle(fontSize: 20),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                );
+              });
+        });
   }
 }
 
@@ -59,36 +62,66 @@ class PositionData {
   final Duration duration;
 }
 
-class Controls extends StatelessWidget {
-  const Controls({super.key, required this.audioPlayer});
+class StreamProgressBar extends StatelessWidget {
+  const StreamProgressBar({super.key, required this.player});
 
-  final AudioPlayer audioPlayer;
+  final Player player;
+
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+          player.stream.position,
+          player.stream.buffer,
+          player.stream.duration,
+          (position, bufferedPosition, duration) => PositionData(
+              position, bufferedPosition, duration ?? Duration.zero));
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<PlayerState>(
-        stream: audioPlayer.playerStateStream,
+    return StreamBuilder<PositionData>(
+        stream: _positionDataStream,
         builder: (context, snapshot) {
-          final playerState = snapshot.data;
-          final processingState = playerState?.processingState;
-          final playing = playerState?.playing;
+          final positionData = snapshot.data;
+          return ProgressBar(
+            barHeight: 10,
+            baseBarColor: Colors.grey[200],
+            progressBarColor: Colors.grey[400],
+            thumbColor: Colors.grey,
+            progress: positionData?.position ?? Duration.zero,
+            total: positionData?.duration ?? Duration.zero,
+            onSeek: player.seek,
+          );
+        });
+  }
+}
+
+class Controls extends StatelessWidget {
+  const Controls({super.key, required this.player});
+
+  final Player player;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: player.stream.playing,
+        builder: (context, snapshot) {
+          final playing = snapshot.data;
           if (!(playing ?? false)) {
-            return IconButton(
-              onPressed: audioPlayer.play,
-              icon: const Icon(Icons.play_arrow_rounded),
-              iconSize: 60,
+            return CupertinoButton(
+              onPressed: player.play,
+              child: const Icon(
+                CupertinoIcons.play_arrow_solid,
+                size: 60,
+              ),
             );
-          } else if (processingState != ProcessingState.completed) {
-            return IconButton(
-              onPressed: audioPlayer.pause,
-              icon: const Icon(Icons.pause_rounded),
-              iconSize: 60,
+          } else {
+            return CupertinoButton(
+              onPressed: player.pause,
+              child: const Icon(
+                CupertinoIcons.pause_fill,
+                size: 60,
+              ),
             );
           }
-          return const Icon(
-            Icons.play_arrow_rounded,
-            size: 60,
-          );
         });
   }
 }
@@ -101,168 +134,68 @@ class AudioPlayerScreen extends StatefulWidget {
 }
 
 class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
-  late AudioPlayer _audioPlayer;
-  late ConcatenatingAudioSource _playlist;
-  bool isShuffleEnabled = false;
-  LoopMode loopmode = LoopMode.all;
+  late final Player player = Player();
 
-  Future<void> _loadAudioSources() async {
-    // Load metadata and create audio sources asynchronously.
-    var firstAudioMetadata = await MetadataGod.readMetadata(
-        file: '/home/jinholee/media_repo/audios/happy-day.mp3');
-    var secondAudioMetadata = await MetadataGod.readMetadata(
-        file:
-            '/home/jinholee/Downloads/TheFatRat - The Calling (feat. Laura Brehm).mp3');
-
-    // Construct AudioSource objects after metadata is loaded.
-    _playlist = ConcatenatingAudioSource(children: [
-      AudioSource.file(
-        '/home/jinholee/media_repo/audios/happy-day.mp3',
-        tag: firstAudioMetadata, // this is now a Metadata object, not a Future
-      ),
-      AudioSource.file(
-        '/home/jinholee/Downloads/TheFatRat - The Calling (feat. Laura Brehm).mp3',
-        tag: secondAudioMetadata, // Metadata object
-      ),
-    ]);
-  }
-
-  Stream<PositionData> get _positionDataStream =>
-      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
-          _audioPlayer.positionStream,
-          _audioPlayer.bufferedPositionStream,
-          _audioPlayer.durationStream,
-          (position, bufferedPosition, duration) => PositionData(
-              position, bufferedPosition, duration ?? Duration.zero));
-
-  void onShuffle() async {
-    isShuffleEnabled = !isShuffleEnabled;
-    await _audioPlayer.setShuffleModeEnabled(isShuffleEnabled);
-    setState(() {});
-  }
-
-  void onLoop() async {
-    switch (loopmode) {
-      case LoopMode.all:
-        loopmode = LoopMode.one;
-        break;
-      case LoopMode.one:
-        loopmode = LoopMode.off;
-        break;
-      case LoopMode.off:
-        loopmode = LoopMode.all;
-        break;
-      default:
-        loopmode = LoopMode.all;
-    }
-    await _audioPlayer.setLoopMode(loopmode);
-    setState(() {});
-  }
+  final playlist = Playlist([
+    Media(
+        '/home/jinholee/Downloads/TheFatRat - The Calling (feat. Laura Brehm).mp3'),
+    Media('/home/jinholee/media_repo/audios/happy-day.mp3')
+  ]);
 
   @override
   void initState() {
     super.initState();
-    _audioPlayer = AudioPlayer();
-    _init();
-  }
-
-  Future<void> _init() async {
-    await _loadAudioSources();
-    await _audioPlayer.setLoopMode(LoopMode.all);
-    await _audioPlayer.setAudioSource(_playlist);
+    player.setPlaylistMode(PlaylistMode.loop);
   }
 
   @override
   void dispose() {
-    _audioPlayer.dispose();
+    player.stop();
+    player.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        child: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        StreamBuilder(
-            stream: _audioPlayer.sequenceStateStream,
-            builder: (context, snapshot) {
-              final state = snapshot.data;
-              if (state?.sequence.isEmpty ?? true) {
-                return const SizedBox();
-              }
-              final metadata = state!.currentSource!.tag as Metadata;
-              Widget image = const Text("No picture");
-              if (metadata.picture != null) {
-                image = Image(
-                  width: 250,
-                  height: 250,
-                  image: MemoryImage(metadata.picture!.data),
-                  fit: BoxFit.cover,
-                );
-              }
-              return MetadataWidget(
-                image: image,
-                title: metadata.title ?? "Unknown Soundtrack",
-                artist: metadata.artist ?? "Unknown Artist",
-              );
-            }),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 170),
-          child: StreamBuilder<PositionData>(
-              stream: _positionDataStream,
-              builder: (context, snapshot) {
-                final positionData = snapshot.data;
-                return ProgressBar(
-                  barHeight: 10,
-                  baseBarColor: Colors.grey[200],
-                  bufferedBarColor: Colors.grey[200],
-                  progressBarColor: Colors.grey[400],
-                  thumbColor: Colors.grey,
-                  progress: positionData?.position ?? Duration.zero,
-                  buffered: positionData?.bufferedPosition ?? Duration.zero,
-                  total: positionData?.duration ?? Duration.zero,
-                  onSeek: _audioPlayer.seek,
-                );
-              }),
-        ),
-        SizedBox(
-          width: 400,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                onPressed: onShuffle,
-                icon: isShuffleEnabled
-                    ? const Icon(Icons.shuffle_on_rounded)
-                    : const Icon(Icons.shuffle_rounded),
-                iconSize: 50,
+    return FutureBuilder(
+        future: player.open(playlist, play: false),
+        builder: (context, snapshot) {
+          return Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MetadataWidget(player: player),
+                  SizedBox(
+                      width: 600,
+                      height: 40,
+                      child: StreamProgressBar(
+                        player: player,
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CupertinoButton(
+                        onPressed: player.previous,
+                        child: const Icon(
+                          CupertinoIcons.backward_fill,
+                          size: 50,
+                        ),
+                      ),
+                      Controls(player: player),
+                      CupertinoButton(
+                        onPressed: player.next,
+                        child: const Icon(
+                          CupertinoIcons.forward_fill,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              IconButton(
-                onPressed: _audioPlayer.seekToPrevious,
-                icon: const Icon(Icons.skip_previous_rounded),
-                iconSize: 50,
-              ),
-              Controls(audioPlayer: _audioPlayer),
-              IconButton(
-                onPressed: _audioPlayer.seekToPrevious,
-                icon: const Icon(Icons.skip_next_rounded),
-                iconSize: 50,
-              ),
-              IconButton(
-                onPressed: onLoop,
-                icon: loopmode == LoopMode.all
-                    ? const Icon(Icons.loop_rounded)
-                    : loopmode == LoopMode.one
-                        ? const Icon(Icons.looks_one_rounded)
-                        : const Icon(Icons.stop_rounded),
-                iconSize: 50,
-              ),
-            ],
-          ),
-        )
-      ],
-    )));
+            ),
+          );
+        });
   }
 }
