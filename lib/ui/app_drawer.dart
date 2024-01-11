2e576b4a2593b7f8f74eac4de0ec862f29cd47f5
jinholee <jinholee@student.42seoul.kr>
2024-01-11 22:23:15 +0100

    fix: appdrawer consumer

--- lib/ui/app_drawer.dart
+++ lib/ui/app_drawer.dart
@@ -12,73 +12,72 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeModel>(
-        builder: (context, model, child) => Drawer(
-              width: 250,
-              child: Column(children: [
-                Container(
-                    width: 250,
-                    height: 100,
-                    margin: const EdgeInsets.only(bottom: 10),
-                    color: model.mode == ThemeMode.light
-                        ? const Color.fromRGBO(0xd9, 0xd9, 0xd9, 1)
-                        : Colors.grey.shade800,
-                    child: const Center(
-                      child: Text("SEA:ME",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w600)),
-                    )),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      ListTile(
-                          leading: const Icon(Icons.music_note, size: 30),
-                          title: const Text(
-                            "Music",
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          onTap: () {
-                            Provider.of<AppController>(context, listen: false)
-                                .updatePage(MusicPlayer());
-                            Navigator.pop(context);
-                          }),
-                      ListTile(
-                          leading: const Icon(Icons.movie, size: 30),
-                          title: const Text(
-                            "Video",
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          onTap: () {
-                            Provider.of<AppController>(context, listen: false)
-                                .updatePage(VideoPlayer());
-                            Navigator.pop(context);
-                          }),
-                      ListTile(
-                          leading: const Icon(Icons.time_to_leave, size: 30),
-                          title: const Text(
-                            "Car Info",
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          onTap: () {
-                            Provider.of<AppController>(context, listen: false)
-                                .updatePage(CarInfo());
-                            Navigator.pop(context);
-                          }),
-                      ListTile(
-                          leading: const Icon(Icons.settings, size: 30),
-                          title: const Text(
-                            "Settings",
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          onTap: () {
-                            Provider.of<AppController>(context, listen: false)
-                                .updatePage(Settings());
-                            Navigator.pop(context);
-                          })
-                    ],
+    final themeModel = Provider.of<ThemeModel>(context);
+    return Drawer(
+      width: 250,
+      child: Column(children: [
+        Container(
+            width: 250,
+            height: 100,
+            margin: const EdgeInsets.only(bottom: 10),
+            color: themeModel.mode == ThemeMode.light
+                ? const Color.fromRGBO(0xd9, 0xd9, 0xd9, 1)
+                : Colors.grey.shade800,
+            child: const Center(
+              child: Text("SEA:ME",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+            )),
+        Expanded(
+          child: ListView(
+            children: [
+              ListTile(
+                  leading: const Icon(Icons.music_note, size: 30),
+                  title: const Text(
+                    "Music",
+                    style: TextStyle(fontSize: 30),
                   ),
-                )
-              ]),
-            ));
+                  onTap: () {
+                    Provider.of<AppController>(context, listen: false)
+                        .updatePage(MusicPlayer());
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  leading: const Icon(Icons.movie, size: 30),
+                  title: const Text(
+                    "Video",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  onTap: () {
+                    Provider.of<AppController>(context, listen: false)
+                        .updatePage(VideoPlayer());
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  leading: const Icon(Icons.time_to_leave, size: 30),
+                  title: const Text(
+                    "Car Info",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  onTap: () {
+                    Provider.of<AppController>(context, listen: false)
+                        .updatePage(CarInfo());
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  leading: const Icon(Icons.settings, size: 30),
+                  title: const Text(
+                    "Settings",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  onTap: () {
+                    Provider.of<AppController>(context, listen: false)
+                        .updatePage(Settings());
+                    Navigator.pop(context);
+                  })
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
