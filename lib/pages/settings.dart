0465894d55a78a8dafc8cc69918ec6c5a4e805fe
jinholee <jinholee@student.42seoul.kr>
2024-01-11 19:15:02 +0100

    feat: darktheme on settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -13,52 +13,69 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: CustomScrollView(slivers: [
-        const CupertinoSliverNavigationBar(
-          automaticallyImplyLeading: false,
-          largeTitle: Text("Settings", style: TextStyle(fontSize: 40)),
-        ),
-        SliverFillRemaining(
-          child: Row(children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.3,
-              child: CupertinoListSection.insetGrouped(
-                backgroundColor: const Color.fromRGBO(240, 240, 240, 1),
-                children: [
-                  CupertinoListTile.notched(
-                    title: const Text("Wifi"),
-                    leading: const Icon(Icons.wifi),
-                    onTap: () => debugPrint("Wifi"),
-                  ),
-                  CupertinoListTile.notched(
-                    title: const Text("Bluetooth"),
-                    leading: const Icon(Icons.bluetooth),
-                    onTap: () => debugPrint("Bluetooth"),
-                  ),
-                  CupertinoListTile.notched(
-                    title: const Text("Display"),
-                    leading: const Icon(Icons.light_mode_outlined),
-                    onTap: () => debugPrint("Display"),
-                  ),
-                  CupertinoListTile.notched(
-                    title: const Text("Theme"),
-                    leading: const Icon(Icons.dark_mode),
-                    onTap: () => Provider.of<ThemeModel>(context, listen: false)
-                        .toggleTheme(),
-                  ),
-                  CupertinoListTile.notched(
-                    title: const Text("About"),
-                    leading: const Icon(Icons.info_outline),
-                    onTap: () => debugPrint("About"),
-                  ),
-                ],
+    return Consumer<ThemeModel>(builder: (context, model, child) {
+      Color textColor =
+          model.mode == ThemeMode.light ? Colors.black : Colors.white;
+      Color backgroundColor = model.mode == ThemeMode.light
+          ? const Color.fromRGBO(245, 245, 245, 1)
+          : const Color.fromRGBO(60, 60, 60, 1);
+      return CupertinoPageScaffold(
+        child: CustomScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            slivers: [
+              CupertinoSliverNavigationBar(
+                automaticallyImplyLeading: false,
+                largeTitle: Text("Settings",
+                    style: TextStyle(fontSize: 40, color: textColor)),
               ),
-            ),
-            Expanded(child: Center(child: Text("Add some settings here"))),
-          ]),
-        )
-      ]),
-    );
+              SliverFillRemaining(
+                child: Row(children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: CupertinoListSection.insetGrouped(
+                      backgroundColor: backgroundColor,
+                      children: [
+                        CupertinoListTile.notched(
+                          title:
+                              Text("Wifi", style: TextStyle(color: textColor)),
+                          leading: const Icon(Icons.wifi),
+                          onTap: () => debugPrint("Wifi"),
+                        ),
+                        CupertinoListTile.notched(
+                          title: Text("Bluetooth",
+                              style: TextStyle(color: textColor)),
+                          leading: const Icon(Icons.bluetooth),
+                          onTap: () => debugPrint("Bluetooth"),
+                        ),
+                        CupertinoListTile.notched(
+                          title: Text("Display",
+                              style: TextStyle(color: textColor)),
+                          leading: const Icon(Icons.light_mode_outlined),
+                          onTap: () => debugPrint("Display"),
+                        ),
+                        CupertinoListTile.notched(
+                          title:
+                              Text("Theme", style: TextStyle(color: textColor)),
+                          leading: const Icon(Icons.dark_mode),
+                          onTap: () =>
+                              Provider.of<ThemeModel>(context, listen: false)
+                                  .toggleTheme(),
+                        ),
+                        CupertinoListTile.notched(
+                          title:
+                              Text("About", style: TextStyle(color: textColor)),
+                          leading: const Icon(Icons.info_outline),
+                          onTap: () => debugPrint("About"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Center(child: Text("Add some settings here"))),
+                ]),
+              )
+            ]),
+      );
+    });
   }
 }
