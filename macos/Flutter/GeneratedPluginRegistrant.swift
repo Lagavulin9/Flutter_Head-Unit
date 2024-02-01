fc726502b75c2fa6cdfc735650a52b71bf41b622
jinholee <jinholee@student.42seoul.kr>
2024-02-01 21:24:06 +0100

    Revert "feat: videoapp with elinux plugin"
    This reverts commit a5d3273239f689c492e2ca894eff42f8543b3c8f.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import media_kit_libs_macos_audio
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitLibsMacosAudioPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosAudioPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
