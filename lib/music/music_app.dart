2b4f76194aba59b7f3dfe2ca88f728339af540fc
jinholee <jinholee@student.42seoul.kr>
2024-02-06 23:12:23 +0100

    fix:media path

--- lib/music/music_app.dart
+++ lib/music/music_app.dart
@@ -22,8 +22,8 @@ class _MusicAppState extends State<MusicApp> {
   List<String> files = [];
 
   Future<List<String>> loadFiles() async {
-    var result = await Process.run(
-        'find', ['/media', '/home', '-type', 'f', '-name', '*.mp3']);
+    var result = await Process.run('find',
+        ['/run/media', '/usr/share/flutter', '-type', 'f', '-name', '*.mp3']);
     if (result.exitCode != 0) {
       debugPrint("Error occured: ${result.exitCode}");
       debugPrint(result.stderr);
