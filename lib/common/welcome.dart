c14ad502e2488b25cad9339e25e50ec22c434600
jinholee <jinholee@student.42seoul.kr>
2024-02-07 22:18:44 +0100

    fix:emoji char to png

--- lib/common/welcome.dart
+++ lib/common/welcome.dart
@@ -5,10 +5,20 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text(
-      "Moin! 👋",
-      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+    return Center(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Moin!  ",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Image.asset(
+          'assets/images/waving_hand.png',
+          width: 40,
+          height: 40,
+        )
+      ],
     ));
   }
 }
