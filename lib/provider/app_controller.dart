03ff2018deedabfe77c52e866bd5cf5bcd6a7abf
jinholee <jinholee@student.42seoul.kr>
2024-03-07 17:33:44 +0100

    Revert "added pdc layout"
    This reverts commit b8217bf75af97bc60d80555bba3ce5f202a7c893.

--- lib/provider/app_controller.dart
+++ lib/provider/app_controller.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_head_unit/common/welcome.dart';
-import 'package:flutter_head_unit/pdc/sensor_view.dart';
 
 class AppController extends ChangeNotifier {
-  Widget _currentPage = const SensorView();
+  Widget _currentPage = const WelcomePage();
 
   Widget get currentPage => _currentPage;
 
